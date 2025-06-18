7f8ca72f0fe6be0064ee2f78791dcdf7d4ae6a22
Aashutosh Parajuli <aashutosh282@gmail.com>
2025-06-18 06:40:24 +0545

    refactor: update ping endpoint to return ApiResponse

--- src/main/java/com/backend/hrms/controller/PingController.java
+++ src/main/java/com/backend/hrms/controller/PingController.java
@@ -3,11 +3,13 @@ package com.backend.hrms.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.backend.hrms.dto.ApiResponse.ApiResponse;
+
 @RestController
 public class PingController {
 
     @GetMapping("/ping")
-    public String ping() {
-        return "pong";
+    public ApiResponse<String> ping() {
+        return new ApiResponse<>(true, "Ping successful", "");
     }
 }

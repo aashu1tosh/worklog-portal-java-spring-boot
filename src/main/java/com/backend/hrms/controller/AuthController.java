e1628975a07b33b6e9210853a47143ed02844175
Aashutosh Parajuli <aparajuli1@hamropatro.com>
2025-06-19 10:44:45 +0545

    feat: update AuthController to return ApiResponse for login and register endpoints

--- src/main/java/com/backend/hrms/controller/AuthController.java
+++ src/main/java/com/backend/hrms/controller/AuthController.java
@@ -1,16 +1,27 @@
 package com.backend.hrms.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.hrms.dto.ApiResponse.ApiResponse;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     @PostMapping("/public/login")
-    public String login() {
+    public ApiResponse<String> login() {
         System.out.println("Login endpoint hit");
-        return "Login endpoint is not implemented yet.";
+        return new ApiResponse<String>(true, "Login endpoint is not implemented yet.", "");
+    }
+
+    @PostMapping("/public/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<String> register() {
+        System.out.println("Register endpoint hit");
+        return new ApiResponse<String>(true, "Register endpoint is not implemented yet.", "");
     }
 }

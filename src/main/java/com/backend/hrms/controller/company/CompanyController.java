e0a48b89ddda9a872fd217f5cce0c02d1969b553
Aashutosh Parajuli <aparajuli1@hamropatro.com>
2025-06-20 15:38:49 +0545

    fix: update authorization to include ADMIN role in createCompany method

--- src/main/java/com/backend/hrms/controller/company/CompanyController.java
+++ src/main/java/com/backend/hrms/controller/company/CompanyController.java
@@ -3,7 +3,6 @@ package com.backend.hrms.controller.company;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,19 +24,15 @@ public class CompanyController {
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    @PreAuthorize("hasAnyAuthority('SUDO_ADMIN')") 
+    @PreAuthorize("hasAnyAuthority('SUDO_ADMIN', 'ADMIN')") 
     public ApiResponse<String> createCompany(@Valid @RequestBody CompanyDTO.Create body,
             @AuthenticationPrincipal JwtPayload jwt1) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        String userId = (String) auth.getPrincipal(); // ← ID
-        String role = auth.getAuthorities()
-                .stream().findFirst()
-                .map(GrantedAuthority::getAuthority)
-                .orElse("ROLE_UNKNOWN");
+        String userId = (String) auth.getPrincipal();
 
-        System.out.println("User ID : " + userId);
-        System.out.println("Role    : " + role);
+        System.out.println("User ID: " + userId);
+        
 
         return new ApiResponse<>(true, Messages.CREATED, "");
     }

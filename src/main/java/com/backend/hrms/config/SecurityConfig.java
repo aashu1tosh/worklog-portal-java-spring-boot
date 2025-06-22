73bde351fcd8be92b33fa55d420f89a021bc03b6
Aashutosh Parajuli <aashutosh282@gmail.com>
2025-06-22 20:13:29 +0545

    feat: enhance CORS configuration to allow specific methods and headers

--- src/main/java/com/backend/hrms/config/SecurityConfig.java
+++ src/main/java/com/backend/hrms/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.backend.hrms.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,10 +36,19 @@ public class SecurityConfig {
         return http
                 /* --- basics -------------------------------------------------- */
                 .csrf(csrf -> csrf.disable()) // stateless API → CSRF not needed
-
+                .cors(cors -> cors
+                        .configurationSource(request -> {
+                            var config = new org.springframework.web.cors.CorsConfiguration();
+                            config.setAllowedOrigins(List.of("http://localhost:5173"));
+                            config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                            config.setAllowedHeaders(List.of("*"));
+                            config.setAllowCredentials(true);
+                            return config;
+                        }))
                 /* --- authorization rules ------------------------------------ */
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/error/**", "/error**").permitAll() // allow error handling
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(publicEndpoints).permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)

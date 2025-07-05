7882b66badbe68a042c05f3b7a660a3e44f58bb9
Aashutosh Parajuli <aashutosh282@gmail.com>
2025-07-05 08:35:55 +0545

    refactor: remove unused import of CompanyAdminDTO in CompanyEmployeeController

--- src/main/java/com/backend/hrms/controller/company/CompanyEmployeeController.java
+++ src/main/java/com/backend/hrms/controller/company/CompanyEmployeeController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.hrms.dto.apiResponse.ApiResponse;
-import com.backend.hrms.dto.company.CompanyAdminDTO;
 import com.backend.hrms.dto.company.CompanyEmployeeDTO;
 import com.backend.hrms.dto.paginatedResponse.PaginatedResponse;
 import com.backend.hrms.entity.company.CompanyEmployeeEntity;
